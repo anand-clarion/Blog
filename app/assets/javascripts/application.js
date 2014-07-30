24e507bc447789b79df25e806d4f425cd3586a40
Anand Tripathi <anand.tripathi@clariontechnologies.co.in>
2014-07-30 10:45:41 +0530

    Added bootstrap and modified css files

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,8 +10,10 @@
 // Read Sprockets README (https://github.com/sstephenson/sprockets#sprockets-directives) for details
 // about supported directives.
 //
+//= require bootstrap.min
 //= require jquery
 //= require jquery_ujs
+//= require bootsy
 //= require turbolinks
 //= require_tree .
 $(document).on('ready page:load', function () {
