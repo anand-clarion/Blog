96e9b41563368fb1701c754986fe1dc108c06082
Anand Tripathi <anand.tripathi@clariontechnologies.co.in>
2014-08-01 16:24:39 +0530

    Added email functionality for user's account activation/deactivation notification

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -7,4 +7,19 @@ class UserMailer < ActionMailer::Base
     @url  = 'http://blog.com/login'
     mail(to: @user.email, subject: 'Welcome to My Awesome Site')
   end
+
+  # This action send a email to user after user account activation
+  def account_activated(user)
+    @user = user
+    @url  = 'http://blog.com/login'
+    mail(to: @user.email, subject: 'Congrat your account has been activated')
+  end
+
+  #This action send a email to user after user account deactivation
+  def account_deactivated(user)
+    @user = user
+    @url  = 'http://blog.com/login'
+    mail(to: @user.email, subject: 'Your account has been De-activated from blog.com')
+  end
+  
 end
